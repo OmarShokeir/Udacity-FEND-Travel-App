bbd21223e986201acd9b725338cf150c68e945dc
OmarShokeir <72025202+OmarShokeir@users.noreply.github.com>
2021-08-20 18:56:14 +0200

    Update app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,12 +1,22 @@
 /* Global Variables */
 let preZip = 'http://api.geonames.org/searchJSON?q=';
 let postZip = '&maxRows=10&username=omarshokeir';
+
+let preWeather = "https://api.weatherbit.io/v2.0/current?lat="
+let postLat = "&lon="
+let weatherKey = "&key=41556fc3f18b4782b005eb7e28385b62"
+
+let predictedWeatherBase = "https://api.weatherbit.io/v2.0/forecast/daily?lat="
+let predictedPostLat = "&lon="
+
+
 // Create a new date instance dynamically with JS
 
+const oneDay = 24 * 60 * 60 * 1000;
+const today = new Date();
 
 
-let d = new Date();
-let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
+
 
 document.getElementById('generate').addEventListener('click', performAction);
 
@@ -14,6 +24,7 @@ function performAction(e) {
     e.preventDefault();
     const zipCode = document.getElementById('zip').value;
     const tripDate = document.getElementById('date').value;
+    const diffDays = Math.round(Math.abs((today - tripDate) / oneDay));
     getData(preZip, zipCode, postZip)
         .then(function (data) {
             // Add data
@@ -28,6 +39,16 @@ function performAction(e) {
 // Async function to get the data from the API
 const getData = async (preZip, zip, postZip) => {
     const res = await fetch(preZip + zip + postZip)
+    try {
+        const data = await res.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+}
+const getCurrentWeather = async (preWeather, lat, postLat, lon, weatherKey) => {
+    const res = await fetch(preWeather + lat + postLat + lon + weatherKey)
     try {
         const data = await res.json();
         console.log(data);
