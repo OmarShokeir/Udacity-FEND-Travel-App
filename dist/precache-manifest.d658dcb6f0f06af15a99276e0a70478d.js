8500f61dd01e34d16aa837155d34866b3bd866fa
OmarShokeir <72025202+OmarShokeir@users.noreply.github.com>
2021-09-03 01:35:22 +0200

    Sorted error with webpack

---
+++ dist/precache-manifest.d658dcb6f0f06af15a99276e0a70478d.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54caeb27bd168657d7e2c551115b2ef1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "87b8a434b2801ba2d6a2",
+    "url": "main.css"
+  },
+  {
+    "revision": "87b8a434b2801ba2d6a2",
+    "url": "main.js"
+  }
+]);
